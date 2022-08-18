01011a65d6c78389ffe9fca9e20bfc3f45c17dad
aitheenti <navneelkuppili@gmail.com>
2022-08-18 00:37:04 -0400

    08/18

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = (str, repeatNumOfTimes) => {
+    if (repeatNumOfTimes > 0) {
+        return str.repeat(repeatNumOfTimes)
+    } else if (repeatNumOfTimes === 0) {
+        return '';
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
