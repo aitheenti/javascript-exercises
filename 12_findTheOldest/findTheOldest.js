23c5d03b99a866fe79b719bd76ae755bfc1141d1
aitheenti <navneelkuppili@gmail.com>
2023-08-31 22:06:03 -0400

    oldest find

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,21 @@
-const findTheOldest = function (people) {
-    console.log(people)
-    const personDetails = []
-    const personFinalObjects = people.map(person => personDetails.push(person.name, person.yearOfDeath - person.yearOfBirth))
-    console.log(personDetails)
+function findTheOldest(peopleArray) {
+    let oldestPerson = null;
+    let maxAge = -1;
+    const currentYear = new Date().getFullYear();
+    for (const person of peopleArray) {
+        const birthYear = person.yearOfBirth;
+        const deathYear = person.yearOfDeath || currentYear;
 
-};
+        const age = deathYear - birthYear;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldestLivingPerson = person;
+        }
+    }
+
+    return oldestLivingPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
